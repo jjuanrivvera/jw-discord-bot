cc7a6e51afd4e8a982568e46a65170db2a6f03a7
jjuanrivvera99 <juan.rivera@alegra.com>
2020-11-23 13:57:35 -0500

    Topic command added

---
+++ Commands/Topic.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
+    let topicEmbed = new Discord.MessageEmbed().setColor("0x1D82B6");
+    if (!args.length) {
+        message.channel.send("Tienes que espec[ificar un tema");
+        return;
+    }
+
+    let topic = args.join('+');
+
+    topicEmbed.setTitle('JW Online Library');
+    topicEmbed.addField(`Aquí puedes encontrar más info acerca de "${topic}"`, `https://wol.jw.org/es/wol/s/r4/lp-s?q=${topic}&p=par&r=occ`);
+
+    message.channel.send(topicEmbed);
+}
+
+module.exports.config = {
+    command: "topic"
+}
